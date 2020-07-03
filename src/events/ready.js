f3c6c1344d99bc1842a402fdace46a107b70af14
JustSomeoneOnGithub <59625582+JustSomeoneOnGithub@users.noreply.github.com>
2020-07-03 11:24:46 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -7,13 +7,11 @@ module.exports = {
         function ping() {
             const pingchannel = Client.channels.cache.get(PINGCHANNELID);
             pingchannel.send("@everyone");
-            console.log("Ping Sent. (Channel 1)");
         }
 
         function ping2() {
             const pingchannel2 = Client.channels.cache.get(PINGCHANNEL2ID);
             pingchannel2.send("@everyone");
-            console.log("Ping Sent. (Channel 2)");
         }
 
         setInterval(ping, 5000);
